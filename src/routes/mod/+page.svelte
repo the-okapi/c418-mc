c8dd353614b49a7b190a988b1a26544e9143bce7
the-okapi <156860750+the-okapi@users.noreply.github.com>
2025-03-31 12:53:25 -0700

    Adding mod link

---
+++ src/routes/mod/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import '$lib/app.css';
+
+    let a = $state();
+</script>
+
+<a href="/c418-1.0.0.jar" hidden bind:this={a}>Download JAR file</a>
+
+<button onclick={() => a.click()}>Download JAR File</button>
+
+<style>
+    :root {
+        background-color: #e80200;
+    }
+    button {
+        background-color: white;
+        color: black;
+        font-family: 'Minecraft Seven';
+        border: none;
+        padding: 2vh 2vw;
+        font-size: 1.25em;
+        border-radius: 2vh;
+        cursor: pointer;
+    }
+    button:hover {
+        background-color: gainsboro;
+    }
+</style>
